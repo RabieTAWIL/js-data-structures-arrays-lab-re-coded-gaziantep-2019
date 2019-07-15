950db1b50bd86d0ce0aa3d4c939043ffd1a6ca42
Rabie TAWIL <rabietawil@gmail.com>
2019-07-15 11:30:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution here!
+
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(){
+  drivers.push("Ralph");
+}
+function destructivelyPrependDriver(){
+  drivers.unshift("Bob");
+}
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+function appendDriver(name){
+  name = [...drivers,"Broom"];
+  return name;
+}
+function prependDriver(name){
+  name = ["Arnold",...drivers];
+  return name;
+}
